e8336dcd5e99a382f2a35b409902455c9595ea03
Ed <edmundho9@gmail.com>
2018-06-07 09:58:19 -0700

    Seed heroku database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 User.destroy_all
 
 User.create(username: 'eho', password: 'fayefaye', first_name: 'Ed', last_name: 'H', birth_date: '1990-11-28')
-User.create(username: 'demouser', password: 'password', first_name: "demo user" last_name: "demo user", birth_date: '2000-01-01')
+User.create(username: 'demouser', password: 'password', first_name: "demo user", last_name: "demo user", birth_date: '2000-01-01')
