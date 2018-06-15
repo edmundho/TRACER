5032c4ed9de47617c1b895d0a25178268449eb46
Ed <edmundho9@gmail.com>
2018-06-15 09:39:59 -0700

    Add final styling to this week stats sidebar

--- frontend/components/past_week_stats.jsx
+++ frontend/components/past_week_stats.jsx
@@ -52,17 +52,17 @@ class PastWeekStats extends React.Component {
     let overallDuration = new Date(null);
     overallDuration.setHours(0);
     overallDuration.setSeconds(overallStats.duration);
-    overallDuration = overallDuration.toTimeString().slice(0, 8);
+    overallDuration = overallDuration.toTimeString().slice(0, 5);
 
     let ridesDuration = new Date(null);
     ridesDuration.setHours(0);
     ridesDuration.setSeconds(rideStats.duration);
-    ridesDuration = ridesDuration.toTimeString().slice(0, 8);
+    ridesDuration = ridesDuration.toTimeString().slice(0, 5);
 
     let runsDuration = new Date(null);
     runsDuration.setHours(0);
     runsDuration.setSeconds(runStats.duration);
-    runsDuration = runsDuration.toTimeString().slice(0, 8);
+    runsDuration = runsDuration.toTimeString().slice(0, 5);
 
    
 
@@ -72,19 +72,19 @@ class PastWeekStats extends React.Component {
           <h3>OVERALL</h3>
           <p>{Number(overallStats.distance).toFixed(1)} mi. covered</p>
         <ul>
-          <li><p>Moving Time</p>{overallDuration}</li>
+          <li><p>Moving Time</p>{`${overallDuration.slice(0, 2)}h ${overallDuration.slice(3, 5)}m`}</li>
           <li><p>Elevation Gained</p>{overallStats.elevation} ft.</li>
         </ul>
         <h3><i className="fas fa-bicycle"></i></h3>
           <p>{Number(rideStats.distance).toFixed(1)} mi. covered</p>
         <ul>
-          <li><p>Moving Time</p>{ridesDuration}</li>
+          <li><p>Moving Time</p>{`${ridesDuration.slice(0, 2)}h ${ridesDuration.slice(3, 5)}m`}</li>
           <li><p>Elevation Gained</p>{rideStats.elevation} ft.</li>
         </ul>
           <h3><i className="fas fa-walking"></i></h3>
           <p>{Number(runStats.distance).toFixed(1)} mi. covered</p>
         <ul>
-          <li><p>Moving Time</p>{runsDuration}</li>
+          <li><p>Moving Time</p>{`${runsDuration.slice(0, 2)}h ${runsDuration.slice(3, 5)}m`}</li>
           <li><p>Elevation Gained</p>{runStats.elevation} ft.</li>
         </ul>
       </div>
