60bb9ba935d43ea5dd7e4ac19e949c272bd1c7a1
eho <edmund@autolist.com>
2019-10-11 17:14:04 -0700

    refactor: RoutesIndexItem

--- frontend/components/routes_index_and_components/routes_index_item.jsx
+++ frontend/components/routes_index_and_components/routes_index_item.jsx
@@ -13,39 +13,45 @@ const styles = StyleSheet.create({
 class RoutesIndexItem extends React.Component {
   constructor (props) {
     super(props);
-
   }
 
   render () {
-    const distance = Number((this.props.route.distance * 0.0006).toFixed(2));
-    const elevation = Number((this.props.route.elevation * 3.28).toFixed());
-    let description;
-    if (this.props.route.description) {
-      description = this.props.route.description;
-    } 
-    const dateCreated = new Date(this.props.route.createdAt).toDateString();
+    const { distance, elevation } = this.getDistanceAndElevation();
+    const { title, description, createdAt, polylineString, origin, destination } = this.props.route;
 
     return (
       <li id="routes-index-item" className={css(styles.fadeIn)}>
         <div>
           <ul>
             <img id="route-image" 
-              src={imageUrlBuilder(
-                this.props.route.polylineString, 
-                this.props.route.origin, 
-                this.props.route.destination, 
-                'small')} alt=""/>
-            <li><h3>{this.props.route.title}</h3></li>
+              src={imageUrlBuilder(polylineString, origin, destination, 'small')} alt=""/>
+            <li><h3>{title}</h3></li>
             <li id="route-description">{description}</li>
             <ul className="distance-elevation">
               <li id="route-distance">{distance} mi<p>Distance</p></li>
               <li id="route-elevation">{elevation} ft<p>Elevation Gain</p></li>
             </ul>
-            <li id="date-created">Created on: {dateCreated}</li>
+            <li id="date-created">Created on: {this.formatDateCreated(createdAt)}</li>
           </ul>
         </div>
       </li>
     );
+  }
+
+  formatDateCreated(createdAt) {
+    return new Date(createdAt).toDateString();
+  }
+
+  formatElevation() {
+    return Number((this.props.route.elevation * 3.28).toFixed());
+  }
+
+  formatDistance() {
+    return Number((this.props.route.distance * 0.0006).toFixed(2));
+  }
+
+  getDistanceAndElevation() {
+    return { distance: this.formatDistance(), elevation: this.formatElevation() };
   }
 }
 
